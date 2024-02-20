dd940f20e145318dc8ef9f6ec9b546514b0ef909
Evan Holloway <eholloway00@gmail.com>
2024-02-20 13:50:06 -0800

    completed Restaurant 2

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json());
+
 //TODO: Create your GET Request Route Below: 
 app.get("/restaurants", async (req, res) => {
     const food = await Restaurant.findAll();
     res.json(food);
+});
+
+app.get("/restaurants/:id", async (req, res) => {
+    const id = req.params.id;
+    const restaurant = await Restaurant.findByPk(id);
+    res.json(restaurant);
 })
-
-
 
 module.exports = app;
